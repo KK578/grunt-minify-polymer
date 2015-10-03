5e6093f087eb997d44814eb4a4fd8fab309a1160
Kevin Kwan <KK1241@googlemail.com>
2015-10-03 10:13:57 +0100

    Enable test suites to handle empty files.

--- test/lib/util.js
+++ test/lib/util.js
@@ -6,11 +6,20 @@ exports.testDirectory = function (test, dir, callback) {
         test.expect(files.length);
 
         for (var i = 0; i < files.length; i++) {
-            var actual = fs.readFileSync(path.join('tmp/', dir, files[i]),
+            var file = files[i];
+
+            if (file.split('.')[0] === 'empty') {
+                // The attempt to read the file should throw an error.
+                test.throws(fs.readFileSync.bind(this, path.join('tmp/', dir, files[i]),
+                { encoding: 'utf-8' }));
+            }
+            else {
+                var actual = fs.readFileSync(path.join('tmp/', dir, files[i]),
                 { encoding: 'utf-8' });
-            var expected = fs.readFileSync(path.join('test/expected/', dir, files[i]),
+                var expected = fs.readFileSync(path.join('test/expected/', dir, files[i]),
                 { encoding: 'utf-8' });
-            test.equal(actual, expected, 'should describe ' + files[i] + ' minified.');
+                test.equal(actual, expected, 'should describe ' + files[i] + ' minified.');
+            }
         }
 
         callback();
